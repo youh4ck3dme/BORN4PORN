3ce2085c06f3b03471c2ce36f2c4327c951cdc1e
ENZO F <enzoenzof2024@gmail.com>
2025-10-20 16:06:33 +0000

    nastav najyssiu kvalitu obrazu / grafiku a najvyssiu upravu kvalityy fon

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    quality: 100,
     remotePatterns: [
       {
         protocol: 'https',
