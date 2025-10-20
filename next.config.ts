44668fb68843884600009d3ce193f1608c4b0e84
ENZO F <enzoenzof2024@gmail.com>
2025-10-20 16:13:10 +0000

    "Analyzuj celý projekt a vykonaj úpravy na dosiahnutie najvyššej možnej

--- next.config.ts
+++ next.config.ts
@@ -7,12 +7,6 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
         hostname: 'images.unsplash.com',
         port: '',
         pathname: '/**',
