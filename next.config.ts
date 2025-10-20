84707cbc0d7d21f020b0967e2b70f4593ef46c09
ENZO F <enzoenzof2024@gmail.com>
2025-10-20 20:50:31 +0000

    editorSimpleWorker.esm.js:1  Failed to load resource: the server respond

--- next.config.ts
+++ next.config.ts
@@ -7,12 +7,6 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
